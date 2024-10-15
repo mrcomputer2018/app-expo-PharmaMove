c42871a31e5eca56a2673c476f2d438915439298
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-10-15 15:51:36 -0300

    Refactor: Update Home component to remove unnecessary background color

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -18,10 +18,7 @@ export default function Home() {
             <Header data={ user as unknown as User | null } 
             signOut={ signOut }/>}
 
-            <View style={ [globalStyles.container, {
-                    backgroundColor: "#f8f9fa",
-                } 
-            ]}>
+            <View style={ globalStyles.container }>
 
             </View>
             
