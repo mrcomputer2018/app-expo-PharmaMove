e401e9b545772b6f627f15199020ea6b7f53b797
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-10-27 06:35:22 -0300

    fix: acertos de layout do mapa

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -10,65 +10,65 @@ export default function Map(props: IMovement) {
     const { origem } = props.route.params.item
     const { destino } = props.route.params.item
     
-    const [latitude, setLatitude] = useState(0)
-    const [longitude, setLongitude] = useState(0)
+    const [latitude, setLatitude] = useState(origem.latitude)
+    const [longitude, setLongitude] = useState(origem.longitude)
 
     return (
         <SafeAreaView style={styles.container}>
             { 
-                (latitude && longitude) ? 
-                (
-                <MapView 
-                    style={{ flex: 1, width: '100%' }}
-                    initialRegion={{
-                        latitude: (origem.latitude + destino.latitude) / 2,
-                        longitude: (origem.longitude + destino.longitude) / 2,
-                        latitudeDelta: Math.abs(origem.latitude - destino.latitude) * 2,
-                        longitudeDelta: Math.abs(origem.longitude - destino.longitude) * 2
-                    }}
-                >
-                    <Marker 
-                        coordinate={{
-                            latitude: destino.latitude,
-                            longitude: destino.longitude
-                        }}
-                        title={destino.nome}
-                        description="Destino da entrega"
-                    />
+            (latitude && longitude) ? 
+            (
+            <MapView 
+                style={{ flex: 1, width: '100%' }}
+                initialRegion={{
+                latitude: (origem.latitude + destino.latitude) / 2,
+                longitude: (origem.longitude + destino.longitude) / 2,
+                latitudeDelta: Math.abs(origem.latitude - destino.latitude) * 1.8,
+                longitudeDelta: Math.abs(origem.longitude - destino.longitude) * 1.8
+                }}
+            >
+                <Marker 
+                coordinate={{
+                    latitude: destino.latitude,
+                    longitude: destino.longitude
+                }}
+                title={destino.nome}
+                description="Destino da entrega"
+                />
 
-                    <Marker 
-                        coordinate={{
-                            latitude: origem.latitude,
-                            longitude: origem.longitude
-                        }}
-                        title={origem.nome}
-                        description="Origem da entrega"
-                    />
+                <Marker 
+                coordinate={{
+                    latitude: origem.latitude,
+                    longitude: origem.longitude
+                }}
+                title={origem.nome}
+                description="Origem da entrega"
+                />
 
-                    <Polyline
-                        coordinates={[
-                            { 
-                                latitude: origem.latitude, 
-                                longitude: origem.longitude 
-                            },
-                            { 
-                                latitude: destino.latitude, 
-                                longitude: destino.longitude 
-                            }
-                        ]}
-                        strokeColor="red"
-                        strokeWidth={3}
-                    />
-                </MapView>
-                ) : 
-                (
-                    <View>
-                        <ActivityIndicator size={48} color='#004085'/>
-                        <Text style={ styles.textLoading }>
-                            Carregando...
-                        </Text>
-                    </View>
-                )
+                <Polyline
+                coordinates={[
+                    { 
+                    latitude: origem.latitude, 
+                    longitude: origem.longitude 
+                    },
+                    { 
+                    latitude: destino.latitude, 
+                    longitude: destino.longitude 
+                    }
+                ]}
+                strokeColor="red"
+                strokeWidth={3}
+                />
+            </MapView>
+            ) : 
+            (
+                <View>
+                <ActivityIndicator size={48} color='#004085'/>
+                <Text style={ styles.textLoading }>
+                    Carregando...
+                </Text>
+                </View>
+            )
             }
         </SafeAreaView>
     )
