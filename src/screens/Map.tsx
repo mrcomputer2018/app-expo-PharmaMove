240b1d47d25f9865d0eb4f7c39a0ecd40b51461e
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-10-28 05:39:44 -0300

    fix: separacao do arquivo de estilos de maps

--- src/screens/Map.tsx
+++ src/screens/Map.tsx
@@ -3,6 +3,7 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import MapView, { Marker, Polyline, PROVIDER_GOOGLE } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import ActivityIndicator from "react-native-paper/src/components/ActivityIndicator";
+import {mapStyles as styles } from '../styles/mapStyles';
 
 interface MapProps {
   route: {
@@ -48,96 +49,61 @@ export default function Map(props: MapProps) {
   }, [coordinates]);
 
   return (
-    <SafeAreaView style={styles.container}>
-      {origem.latitude && origem.longitude && destino.latitude && destino.longitude ? (
-        <View style={styles.mapContainer}>
-          <MapView
-            style={styles.map}
-            ref={mapEl}
-            provider={PROVIDER_GOOGLE}
-            initialRegion={{
-              latitude: (origem.latitude + destino.latitude) / 2,
-              longitude: (origem.longitude + destino.longitude) / 2,
-              latitudeDelta: Math.abs(origem.latitude - destino.latitude) * 1.8,
-              longitudeDelta: Math.abs(origem.longitude - destino.longitude) * 1.8,
-            }}
-          >
-            <Marker 
-              coordinate={{ latitude: origem.latitude, longitude: origem.longitude }}
-              title={origem.nome}
-              description="Origem da entrega"
-            />
-            <Marker 
-              coordinate={{ latitude: destino.latitude, longitude: destino.longitude }}
-              title={destino.nome}
-              description="Destino da entrega"
-            />
-            
-            {coordinates.length > 0 && (
-              <Polyline coordinates={coordinates} strokeColor="red" strokeWidth={4} />
-            )}
-
-            <MapViewDirections
-              origin={{ latitude: origem.latitude, longitude: origem.longitude }}
-              destination={{ latitude: destino.latitude, longitude: destino.longitude }}
-              apikey={process.env.GOOGLE_MAPS_API_KEY || 'AIzaSyDHSmaN9oYe9ddQDgb4aozgDZepEKtSaMk'}
-              strokeColor="blue"
-              strokeWidth={4}
-              onReady={result => {
-                setDistance(result.distance);
-                setDuration(result.duration);
-                setCoordinates(result.coordinates);
+        <SafeAreaView style={styles.container}>
+        {origem.latitude && origem.longitude && destino.latitude && destino.longitude ? (
+          <View style={styles.mapContainer}>
+            <MapView
+              style={styles.map}
+              ref={mapEl}
+              provider={PROVIDER_GOOGLE}
+              initialRegion={{
+                latitude: (origem.latitude + destino.latitude) / 2,
+                longitude: (origem.longitude + destino.longitude) / 2,
+                latitudeDelta: Math.abs(origem.latitude - destino.latitude) * 1.8,
+                longitudeDelta: Math.abs(origem.longitude - destino.longitude) * 1.8,
               }}
-              onError={error => console.error('Erro ao calcular a rota:', error)}
-            />
-          </MapView>
-          <View style={styles.infoContainer}>
-            <Text style={styles.textInfo}>Distância: {(distance ?? 0).toFixed(2)} km</Text>
-            <Text style={styles.textInfo}>Tempo estimado: {((duration ?? 0) / 60).toFixed(2)} horas</Text>
+            >
+              <Marker 
+                coordinate={{ latitude: origem.latitude, longitude: origem.longitude }}
+                title={origem.nome}
+                description="Origem da entrega"
+              />
+              <Marker 
+                coordinate={{ latitude: destino.latitude, longitude: destino.longitude }}
+                title={destino.nome}
+                description="Destino da entrega"
+              />
+              
+              {coordinates.length > 0 && (
+                <Polyline coordinates={coordinates} strokeColor="red" strokeWidth={4} />
+              )}
+
+              <MapViewDirections
+                origin={{ latitude: origem.latitude, longitude: origem.longitude }}
+                destination={{ latitude: destino.latitude, longitude: destino.longitude }}
+                apikey={process.env.GOOGLE_MAPS_API_KEY || 'AIzaSyDHSmaN9oYe9ddQDgb4aozgDZepEKtSaMk'}
+                strokeColor="blue"
+                strokeWidth={4}
+                onReady={result => {
+                  setDistance(result.distance);
+                  setDuration(result.duration);
+                  setCoordinates(result.coordinates);
+                }}
+                onError={error => console.error('Erro ao calcular a rota:', error)}
+              />
+            </MapView>
+            <View style={styles.infoContainer}>
+              <Text style={styles.textInfo}>Distância: {(distance ?? 0).toFixed(2)} km</Text>
+              <Text style={styles.textInfo}>Tempo estimado: {((duration ?? 0) / 60).toFixed(2)} horas</Text>
+            </View>
           </View>
-        </View>
-      ) : (
-        <View style={styles.loadingContainer}>
-          <ActivityIndicator size={48} color='#004085' />
-          <Text style={styles.textLoading}>Carregando...</Text>
-        </View>
-      )}
-    </SafeAreaView>
+        ) : (
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator size={48} color='#004085' />
+            <Text style={styles.textLoading}>Carregando...</Text>
+          </View>
+        )}
+      </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  mapContainer: {
-    flex: 1,
-  },
-  map: {
-    flex: 1,
-    width: '100%',
-  },
-  infoContainer: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    backgroundColor: '#cce5ff',
-  },
-  textInfo: {
-    fontSize: 18,
-    color: '#004085',
-    fontWeight: 'bold',
-    padding: 5,
-  },
-  loadingContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textLoading: {  
-    marginTop: 10,
-    fontSize: 18,
-    color: '#004085',
-    fontWeight: 'bold',
-  },
-});
