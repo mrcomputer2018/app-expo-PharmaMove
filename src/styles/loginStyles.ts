360815663eba660ace140f407a8d480ad3b32eb0
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-10-29 06:44:33 -0300

    feat: Tela de login

--- src/styles/loginStyles.ts
+++ src/styles/loginStyles.ts
@@ -6,6 +6,13 @@ export const loginStyles = StyleSheet.create({
         backgroundColor: '#004085',
         justifyContent: 'center',
     },
+    background: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: 300,
+    },
     errorText: {
         color: 'red',
         marginBottom: 10,
